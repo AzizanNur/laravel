c9974cf28d8ea44b48a42f7de86bab1b7cfe50ab
AzizanNur <zhiz.nevermind@gmail.com>
2022-04-29 13:47:10 +0700

    delete folder unused

--- aplications/tes/coba-laravel-8x/resources/views/about.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.main')
-
-@section('container')
-    <h1>About dengan template blade</h1>
-    <h3>{{ $name }}</h3>
-    <p>{{ $email }}</p>  
-    <img src="img/{{ $image }}" alt="{{ $name }}" class="rounded-circle">  
-@endsection
