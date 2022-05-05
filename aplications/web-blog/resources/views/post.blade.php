fc785399a986b44aa39f440385493752d2865056
AzizanNur <zhiz.nevermind@gmail.com>
2022-05-06 06:59:39 +0700

    changed url category to new url

--- aplications/web-blog/resources/views/post.blade.php
+++ aplications/web-blog/resources/views/post.blade.php
@@ -7,7 +7,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <h1>{{ $post->title }}</h1>
-                    <p>By. <a class='text-decoration-none' href="/user/{{ $post->user->slug }}">{{ $post->user->name }}</a> in <a class='text-decoration-none' href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+                    <p>By. <a class='text-decoration-none' href="/user/{{ $post->user->slug }}">{{ $post->user->name }}</a> in <a class='text-decoration-none' href="/blog?category={{ $post->category->slug }}">{{ $post->category->name }}</a></p>
                     <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" class="card-img-top img-fluid" alt="">
                     
                     <article class="my-3 fs-5">
