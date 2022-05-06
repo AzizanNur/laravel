abf23533ff8fda8960355376213efc4b10dd0540
AzizanNur <zhiz.nevermind@gmail.com>
2022-05-07 06:30:24 +0700

    add fitur registration into admin

--- aplications/web-blog/resources/views/register/index.blade.php
+++ aplications/web-blog/resources/views/register/index.blade.php
@@ -6,18 +6,34 @@
         <div class="col-lg-5">
             <main class="form-registration">
                 <h1 class="h3 mb-3 fw-normal text-center">Registration Form</h1>              
-                <form>
+                <form action="/register" method="POST">
+                  @csrf  
                   <div class="form-floating">
-                    <input type="text" class="form-control rounded-top" name="name" id="name" placeholder="Name">
+                    <input value="{{ old('name') }}" type="text" class="form-control rounded-top @error('name') is-invalid @enderror" name="name" id="name" placeholder="Name" required>
                     <label for="name">Name</label>
+                    @error('name')
+                    <div class="invalid-feedback">
+                      {{ $message }}
+                    </div>
+                    @enderror
                   </div>
                   <div class="form-floating">
-                    <input type="email" name="email" class="form-control" id="email" placeholder="name@example.com">
+                    <input value="{{ old('email') }}" type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="name@example.com" required>
                     <label for="email">Email address</label>
+                    @error('email')
+                    <div class="invalid-feedback">
+                      {{ $message }}
+                    </div>
+                    @enderror
                   </div>
                   <div class="form-floating">
-                    <input type="password" class="form-control rounded-bottom" id="password" placeholder="Password">
+                    <input type="password" name="password" class="form-control rounded-bottom @error('password') is-invalid @enderror" id="password" placeholder="Password" required>
                     <label for="password">Password</label>
+                    @error('password')
+                    <div class="invalid-feedback">
+                      {{ $message }}
+                    </div>
+                    @enderror
                   </div>
               
                   <button class="w-100 btn btn-lg btn-primary mt-3" type="submit">Register</button>                 
