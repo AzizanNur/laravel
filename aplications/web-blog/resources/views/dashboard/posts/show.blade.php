dcd10c66da78e7be38c42f89ce7654a311bad5c3
AzizanNur <zhiz.nevermind@gmail.com>
2022-05-13 09:54:43 +0700

    proses destroy / delete post

--- aplications/web-blog/resources/views/dashboard/posts/show.blade.php
+++ aplications/web-blog/resources/views/dashboard/posts/show.blade.php
@@ -7,7 +7,11 @@
             <h1>{{ $post->title }}</h1>
                 <a href="/dashboard/posts" class="btn btn-success"><span data-feather="arrow-left"></span> Back To All My posts</a>
                 <a href="" class="btn btn-warning"><span data-feather="edit"></span> Edit</a>
-                <a href="" class="btn btn-danger"><span data-feather="x-circle"></span> Delete</a>
+                <form action="/dashboard/posts/{{ $post->slug }}" method="POST" class="d-inline">
+                    @csrf
+                    @method('delete')
+                    <button class="btn btn-danger" onclick="return confirm('Are you sure?')"><span data-feather="x-circle"></span> Delete</button>
+                </form>
                 <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" class="card-img-top img-fluid mt-4" alt="">
                 
                 <article class="my-3 fs-5">
